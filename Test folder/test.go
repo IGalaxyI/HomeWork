eb3d67f60c4346192ff5b25418658dc6392b04ce
IGalaxyI <mansur42915@gmail.com>
2026-07-23 20:33:00 +0500

    Практика: Консольный менеджер энергопотребления «SmartHome»

--- Test folder/test.go
+++ Test folder/test.go
@@ -1,94 +1,60 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-)
+import "fmt"
 
 func main() {
-	ex1()
-	ex2()
-	ex3()
-	ex4()
-	ex5()
-	ex6()
-	ex7()
+	smartHome()
 }
 
-func ex1() {
-	for i := 1; i <= 20; i++ {
-		fmt.Println(i)
-	}
-	fmt.Println("Ex1 End")
-}
-func ex2() {
-	sum := 0
-	for i := 0; i <= 100; i++ {
-		sum += i
-	}
-	fmt.Println(sum)
-	fmt.Println("Ex2 End")
+type Product struct {
+	Name  string
+	Power float64
+	Time  float64
+	Night bool
 }
 
-func ex3() {
-	number := rand.Intn(9) + 1
-	fmt.Println(number)
-	for i := 1; i < 10; i++ {
-		fmt.Println(number * i)
-	}
-	fmt.Println("Ex3 End")
-}
+func smartHome() {
+	const BaseTariff = 0.45
+	const HighLoadTax = 0.15
+	const NightDiscount = 0.30
 
-func ex4() {
-	n := rand.Intn(100)
-	s := []int{}
-	fmt.Println(n)
-	for i := 1; i <= n; i++ {
-		v := i % 3
-		if v == 0 {
-			s = append(s, i)
-		}
-	}
-	fmt.Println(s)
-	fmt.Println("Ex4 End")
-}
+	var products []Product
 
-func ex5() {
-	n := rand.Intn(1000)
-	fmt.Println(n)
-	sum := 0
-	for i := 1; i < n; i++ {
-		n /= 10
-		sum += 1
-	}
-	fmt.Println(sum)
-	fmt.Println("Ex5 End")
-}
-
-func ex6() {
-	text := "TEST"
-	for i := 0; i < len(text); i++ {
-		fmt.Println(string(text[i]))
-	}
-	fmt.Println("Ex6 End")
-}
-
-func ex7() {
-	balance := 3000
 	for true {
-		fmt.Println("Введите 1.2.3")
-		a := 0
-		fmt.Scan(&a)
-
-		if a == 1 {
-			fmt.Println(balance)
-		} else if a == 2 {
-			balance += 500
-		} else if a == 3 {
-			balance -= 200
-		} else if a == 0 {
+		var product Product
+		fmt.Println("Введите название прибора:")
+		fmt.Scan(&product.Name)
+		if product.Name == ("done") {
 			break
 		}
+		fmt.Println("Введите мощьность:")
+		fmt.Scan(&product.Power)
+		fmt.Println("Введите время:")
+		fmt.Scan(&product.Time)
+		fmt.Println("Введите ночной режим true/false:")
+		fmt.Scan(&product.Night)
+		products = append(products, product)
 	}
-	fmt.Println("Ex7 End")
+
+	for _, product := range products {
+		Rashod := (product.Power * product.Time) / 1000
+		Cash := Rashod * BaseTariff
+		category := ""
+		if product.Night {
+			Cash = Cash - (Cash * NightDiscount)
+		}
+		if Rashod > 10 {
+			Cash = Cash + (Cash * HighLoadTax)
+		}
+		switch {
+		case product.Power >= 0 && product.Power < 100:
+			category += "Экономный"
+		case product.Power > 100 && product.Power < 1000:
+			category += "Стандартный"
+		case product.Power > 1000:
+			category += "Мощный"
+		}
+
+		fmt.Printf("--Отчет по прибору--\nПрибор: %s [Категория:%s] \nРасход: %.2f \nИтоговая стоимость: %.2f\n\n", product.Name, category, Rashod, Cash)
+	}
 }
