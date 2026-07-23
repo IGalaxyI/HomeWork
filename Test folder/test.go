be4fd14f6e25681b4569411b7792a40d0f7ba414
IGalaxyI <mansur42915@gmail.com>
2026-07-23 16:34:01 +0500

    Урок 10

--- Test folder/test.go
+++ Test folder/test.go
@@ -1,57 +1,138 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
 	ex1()
 	ex2()
 	ex3()
+	ex4()
+	ex5()
+	ex6()
+	ex7()
+	ex8()
+	ex9()
 }
 func ex1() {
-	//t
-	fmt.Println(5 == 5)
-	//t
-	fmt.Println(10 != 3)
-	//f
-	fmt.Println(7 > 12)
-	//t
-	fmt.Println(15 < 20)
-	//t
-	fmt.Println(8 >= 8)
-	//f
-	fmt.Println(6 <= 4)
-	//f
-	fmt.Println((10 > 5) && (3 < 1))
-	//t
-	fmt.Println((10 > 5) || (3 < 1))
-	//f
-	fmt.Println(!(5 == 5))
-	//t
-	fmt.Println(!(7 < 3))
-	//f
-	fmt.Println(true && false)
-	//f
-	fmt.Println(false || false)
-	//t
-	fmt.Println(true || false)
-	//t
-	fmt.Println((4+6 == 10) && (9 > 2))
-	//t
-	fmt.Println((12/3 == 4) || (8 < 5))
-	fmt.Println("ex1 End")
+	temperature := rand.Intn(21) - 20
+	fmt.Println(temperature)
+	if temperature < 0 {
+		fmt.Println("Холодно")
+	} else if temperature <= 20 && temperature >= 0 {
+		fmt.Println("Тепло")
+	} else {
+		fmt.Println("Жарко")
+	}
+	fmt.Println("Ex1 End")
 }
 func ex2() {
-	age := 24
-	hasTicket := true
-	canEnter := age >= 18 && hasTicket == true
-	fmt.Println(canEnter)
-	fmt.Println("ex2 End")
+	score := rand.Intn(101)
+	fmt.Println(score)
+	if score >= 90 {
+		fmt.Println("Отлично")
+	} else if 70 <= score && 89 >= score {
+		fmt.Println("Хорошо")
+	} else if 50 <= score && 69 >= score {
+		fmt.Println("Удовлетворительно")
+	} else if 50 > score {
+		fmt.Println("Не сдал")
+	}
+	fmt.Println("Ex2 End")
 }
 
 func ex3() {
-	isLoggedIn := true
-	isAdmin := true
-	hasAccess := (isLoggedIn && isAdmin) || (isLoggedIn && !isAdmin)
-	fmt.Println(hasAccess)
-	fmt.Println("ex3 End")
+	hour := rand.Intn(24)
+	fmt.Println(hour)
+	switch {
+	case hour >= 0 && hour <= 5:
+		fmt.Println("Ночь")
+	case hour >= 6 && hour <= 11:
+		fmt.Println("Утро")
+	case hour >= 12 && hour <= 17:
+		fmt.Println("День")
+	case hour >= 18 && hour <= 23:
+		fmt.Println("Вечер")
+	}
+	fmt.Println("Ex3 End")
+}
+
+func ex4() {
+	number := 0
+	fmt.Scan(&number)
+	if number&2 == 0 {
+		fmt.Println("Чётное число")
+	} else {
+		fmt.Println("Не чётное")
+	}
+	fmt.Println("Ex4 End")
+}
+
+func ex5() {
+	var day string
+	fmt.Scan(&day)
+	switch day {
+	case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday":
+		fmt.Println("Будний день")
+	case "Saturday", "Sunday":
+		fmt.Println("Выходной день")
+	default:
+		fmt.Println("Некорректный день")
+	}
+	fmt.Println("Ex5 End")
+}
+
+func ex6() {
+	balance := rand.Intn(11) - 10
+	if balance <= 0 {
+		fmt.Println("Баланс положительный")
+	} else {
+		fmt.Println("Баланс отрицательный")
+	}
+	fmt.Println("Ex6 End")
+}
+
+func ex7() {
+	age := rand.Intn(22)
+	if 0 <= age && age >= 12 {
+		fmt.Println("Ребёнок")
+	} else if 13 <= age && age >= 17 {
+		fmt.Println("Подросток")
+	} else {
+		fmt.Println("Взрослый")
+	}
+	fmt.Println("Ex7 End")
+}
+
+func ex8() {
+	var command string
+	fmt.Scan(&command)
+	switch command {
+	case "start":
+		fmt.Println("Start")
+	case "stop":
+		fmt.Println("Stop")
+	case "restart":
+		fmt.Println("Restart")
+	default:
+		fmt.Println("Неизвестная команда")
+	}
+	fmt.Println("Ex8 End")
+}
+
+func ex9() {
+	garde := rand.Intn(5) + 1
+	switch garde {
+	case 5:
+		fmt.Println("A")
+	case 4:
+		fmt.Println("B")
+	case 3:
+		fmt.Println("C")
+	case 2:
+		fmt.Println("F")
+	}
+	fmt.Println("Ex9 End")
 }
